42b880996e9b230df9deace90d06566d0eaa3a88
HuangQitai <861902610@qq.com>
2019-11-22 10:40:32 +0800

    shiroPermission-demo

---
+++ shiroPermission/src/main/java/com/shiroPermission/mapper/RolePermissionMapper.java
@@ -0,0 +1,21 @@
+package com.shiroPermission.mapper;
+
+import com.shiroPermission.pojo.RolePermission;
+import com.shiroPermission.pojo.RolePermissionExample;
+import java.util.List;
+
+public interface RolePermissionMapper {
+    int deleteByPrimaryKey(Long id);
+
+    int insert(RolePermission record);
+
+    int insertSelective(RolePermission record);
+
+    List<RolePermission> selectByExample(RolePermissionExample example);
+
+    RolePermission selectByPrimaryKey(Long id);
+
+    int updateByPrimaryKeySelective(RolePermission record);
+
+    int updateByPrimaryKey(RolePermission record);
+}
---
+++ shiroPermission/src/main/java/com/shiroPermission/service/PermissionService.java
@@ -0,0 +1,21 @@
+package com.shiroPermission.service;
+ 
+import com.shiroPermission.pojo.Permission;
+import com.shiroPermission.pojo.Role;
+
+import java.util.List;
+import java.util.Set;
+
+ 
+public interface PermissionService {
+    public Set<String> listPermissions(String userName);
+ 
+    public List<Permission> list();
+    public void add(Permission permission);
+    public void delete(Long id);
+    public Permission get(Long id);
+    public void update(Permission permission);
+ 
+    public List<Permission> list(Role role);
+     
+}
